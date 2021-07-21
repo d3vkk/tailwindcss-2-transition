878c318db653816903fad7c9640edd864f7f6d42
d3vkk <donaldkk22@yahoo.com>
2021-07-21 09:41:46 +0300

    feat: Extend color palette

--- dark-mode/tailwind.config.js
+++ dark-mode/tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [
     './public/**/*.html',
@@ -6,7 +8,36 @@ module.exports = {
   mode: 'jit',
   darkMode: 'media',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        white: colors.white,
+        black: colors.black,
+        bluegray: colors.blueGray,
+        coolgray: colors.coolGray,
+        gray: colors.gray,
+        truegray: colors.trueGray,
+        warmgray: colors.warmGray,
+        red: colors.red,
+        orange: colors.orange,
+        amber: colors.amber,
+        yellow: colors.yellow,
+        lime: colors.lime,
+        green: colors.green,
+        emerald: colors.emerald,
+        teal: colors.teal,
+        cyan: colors.cyan,
+        sky: colors.sky,
+        blue: colors.blue,
+        indigo: colors.indigo,
+        violet: colors.violet,
+        purple: colors.purple,
+        fuchsia: colors.fuchsia,
+        pink: colors.pink,
+        rose: colors.rose,
+      },
+    }
   },
   variants: {
     extend: {},
